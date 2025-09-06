54a06594dfc19288dc03f6dc115209b6b0e496a0
punz321 <thakurpankhuri803@gmail.com>
2025-09-06 15:02:18 +0530

    added basic frontend app

---
+++ app.py
@@ -0,0 +1,27 @@
+#frontend app 
+import streamlit as st
+import pandas as pd
+from recom import recommended
+
+@st.cache_data #cache-ing the data so it doesnt load the data everytime the program runs
+def load_data():
+    movies = pd.read_csv("ml-100k\ml-100k\u.item", sep = "|", encoding = "latin-1")
+
+    return movies
+
+movies = load_data
+
+#streamlit UI
+
+st.title("(Alpha))Movie Recommender System🎬🍿")
+st.write("Type a movie name to get recommendations!!")
+
+user_input = st.text_input("Enter a movie title")
+
+if st.button("Recommend"):
+    if user_input:
+        result = recommended(user_input, 5)
+        st.write("### Recommendations:")
+        st.dataframe(result)
+    else:
+        st.write("Please enter a movie title")
