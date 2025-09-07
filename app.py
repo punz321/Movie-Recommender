23144676badd08171eefa0fe76f7fd54a1a44ece
punz321 <thakurpankhuri803@gmail.com>
2025-09-07 21:42:34 +0530

    added a new feature

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #frontend app 
 import streamlit as st
+import requests
 import pandas as pd
 from recom import recommended
 
@@ -10,12 +11,31 @@ st.write("Type a movie name to get recommendations!!")
 
 user_input = st.text_input("Enter a movie title")
 
+#helper function for posters along w fetching the titles
+TMDB_API_KEY = "your_api_key_here"
+
+def get_poster(movie_title):
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={movie_title}"
+    response = requests.get(url).json()
+    if response["results"]:
+        poster_path = response["results"][0]["poster_path"]
+        full_path = f"https://image.tmdb.org/t/p/w500{poster_path}"
+        return full_path
+    return None
+
 if st.button("Recommend"):
     if user_input:
-
         clean_input = user_input.lower().strip()
         result = recommended(clean_input, 5)
         st.write("### Recommendations:")
-        st.write(result)
+
+        for movie in result:
+            poster_url = get_poster(movie)
+        if poster_url:
+            st.image(poster_url, width=150, caption=movie)
+        else:
+            st.write(movie)
     else:
         st.write("Please enter a movie title")
+
+
