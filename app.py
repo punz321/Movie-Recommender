00cf7d8b8c77b049283dd97219f6c9b3705bd632
punz321 <thakurpankhuri803@gmail.com>
2025-09-07 21:13:31 +0530

    redid the thing bc it didn't work :/

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 from recom import recommended
 
 #streamlit UI
-st.markdown("Movie Recommender System")
 st.title("Movie Recommender System🎬🍿")
 st.write("Type a movie name to get recommendations!!")
 
